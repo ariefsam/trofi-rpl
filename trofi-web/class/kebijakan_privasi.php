b6173978c04239a5029a55a0f03ca17c8400da57
ariefhidayatulloh <ariefhidayatulloh@cb847f4b-a55f-4440-e721-11c6dcb2be24>
2010-12-29 02:58:27 +0000

    ada kebijakan privasi

---
+++ trofi-web/class/kebijakan_privasi.php
@@ -0,0 +1,20 @@
+<?php
+class kebijakan_privasi {
+    function tampilkan(){?>
+<h2>Kebijakan Privasi</h2>
+
+<p>Trofi webs&nbsp; adalah pembuat wadah untuk para pengguna melihat harga buah buahan tropis dan dapat memasang iklan serta mengikuti forum di www.trofi-webs. Pernyataan privasi ini merupakan kebijakan privasi ("Kebijakan Privasi") Trofi webs yang berlaku pada informasi yang Anda berikan kepada kami atau yang kami kumpulkan di Situs Web Trofi webs, melalui Dukungan Pelanggan Trofi webs atau melalui pembelian, interaksi Anda dengan, atau penggunaan Anda atas Web ini ("Informasi Pengguna" Anda). Seperti yang dijelaskan lebih lanjut di bawah ini, Informasi Pengguna dapat disertai informasi yang mengidentifikasi Anda secara pribadi ("Informasi yang Dapat Diidentifikasi Secara Pribadi"). Kebijakan Privasi ini menjelaskan cara kami mengumpulkan, menggunakan dan menangani Informasi Pengguna, termasuk Informasi yang Dapat Diidentifikasi Secara Pribadi.</p>
+<p>Hak Anda atas privasi dan keamanan data adalah perhatian utama kami, dan itulah alas an kami berusaha agar pengalaman setiap pengguna aman dan selamat. Dengan mengunjungi Situs Web Trofi webs atau mendaftar dan mengisi forum serta iklan, Anda menerima ketentuan yang dinyatakan dalam Kebijakan Privasi ini. Jika tidak menyetujui Kebijakan Privasi ini, jangan mengunjungi Situs Web Trofi atau mendaftar sebagai member.</p>
+<h3>Seberapa Aman Informasi Tentang Anda?</h3>
+<p>Untuk membantu melindungi Informasi Terkait Pribadi yang dimiliki Trofi webs, kami menggunakan bermacam pengamanan yang memadai, yang meliputi:</p>
+<ul>
+<li>Kontrol teknis, administratif, dan fisik.</li>
+<li>Sistem otorisasi.</li>
+<li>Perjanjian dengan kontraktor dan vendor mengharuskan mereka untuk melindungi kerahasiaan, keselamatan, dan keamanan informasi pengguna.</li>
+
+</ul>
+<p>Trofi webs telah membuat pengamanan ini untuk membantu mencegah akses tanpa hak pada atau penyalahgunaan Informasi Pengguna, tapi tidak menjamin bahwa Informasi Pengguna tidak akan pernah diungkap dengan cara yang tidak sejalan dengan Kebijakan Privasi ini (misalnya, sebagai akibat dari tindakan tanpa hak yang dilakukan oleh pihak ketiga yang melanggar hukum yang berlaku atau kebijakan Trofi webs dan afiliasinya).</p>
+<p>Penghapusan naskah atau tulisan dari proyek-proyek Trofi webs sebenarnya tidak benar-benar menghapusnya. Pada artikel normal, siapapun dapat melihat versi terdahulu dan membaca apa yang pernah ada di sana. Jika suatu artikel "dihapus", seorang&nbsp;<em>sysop</em>/administrator (pengurus) - yang merupakan seorang yang dipercayai oleh hampir semua orang untuk tidak menyalahgunakan kemampuan penghapusannya - dapat melihat apa yang dihapus. Hanya seorang pengembang yang dapat secara permanen menghapus informasi dari trofi webs dan tidak ada jaminan bahwa hal ini akan terjadi, kecuali sebagai jawaban atas tindakan hukum.</p>
+
+    <?php }
+}
