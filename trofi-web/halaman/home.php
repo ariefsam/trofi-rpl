ea75694e0faa4bd76f83cfd3c145500fa40a77ab
caturpurboyuwono <caturpurboyuwono@cb847f4b-a55f-4440-e721-11c6dcb2be24>
2010-12-29 10:11:00 +0000

    terurut pak

--- trofi-web/halaman/home.php
+++ trofi-web/halaman/home.php
@@ -1,7 +1,9 @@
 <?php
+    $tes = mysql_query("select * from halaman");
+    $v = mysql_num_rows($tes);
     $halaman->header->judul = "Beranda : Trofi Webs";
-    $halaman->konten->isi[0] = new getsum(new artikel(3));
-    $halaman->konten->isi[1] = new getsum(new artikel(4));
+    $halaman->konten->isi[0] = new getsum(new artikel($v));
+    $halaman->konten->isi[1] = new getsum(new artikel($v-1));
     //$halaman->konten->isi[2] = new extrapanel();
     $halaman->footer->isi = "Design by Aloha";
 ?>
